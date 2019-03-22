944fb713d877f5d9d93c2669d1718df050914db8
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2019-03-22 11:20:48 +0100

    init

---
+++ lib/graph_of_thrones.ex
@@ -0,0 +1,18 @@
+defmodule GraphOfThrones do
+  @moduledoc """
+  Documentation for GraphOfThrones.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GraphOfThrones.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/graph_of_thrones_test.exs
@@ -0,0 +1,8 @@
+defmodule GraphOfThronesTest do
+  use ExUnit.Case
+  doctest GraphOfThrones
+
+  test "greets the world" do
+    assert GraphOfThrones.hello() == :world
+  end
+end
